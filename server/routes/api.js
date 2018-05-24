bb77967f4b7ca172cac9ca126a5503a069ebe8cf
Sanclayr Vieria Sindarsic <sanclayr@gmail.com>
2018-05-24 16:39:17 -0300

    Added mongoose

--- server/routes/api.js
+++ server/routes/api.js
@@ -1,6 +1,19 @@
 const express = require('express')
 const router = express.Router()
 
+const mongoose = require('mongoose')
+
+const db = "mongodb://sanclayr:passwordsanclayr@ds021999.mlab.com:21999/eventsdb"
+
+
+mongoose.connect(db, err => {
+    if(err){
+        console.error('Error!' + err)
+    } else {
+        console.log('Conected to mongodb')
+    }
+})
+
 router.get('/',(req, res) => {
     res.send('From API route')
 })
