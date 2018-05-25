4392670e22a654a2358177c0b52377f6cc4577d9
Sanclayr Vieira Sindarsic <sanclayr@gmail.com>
2018-05-25 20:51:36 -0300

    Curso Finalizado:  Angular Authentication - Mastering the Basics JWT with mongoose, mongodb, express

--- server/routes/api.js
+++ server/routes/api.js
@@ -1,5 +1,7 @@
 const express = require('express')
+const jwt = require('jsonwebtoken')
 const router = express.Router()
+const User = require('../models/user')
 
 const mongoose = require('mongoose')
 
@@ -7,15 +9,102 @@ const db = "mongodb://sanclayr:passwordsanclayr@ds021999.mlab.com:21999/eventsdb
 
 
 mongoose.connect(db, err => {
-    if(err){
+    if (err) {
         console.error('Error!' + err)
     } else {
         console.log('Conected to mongodb')
     }
 })
 
-router.get('/',(req, res) => {
+function verifyToken(req, res, next) {
+    if (!req.headers.authorization) {
+        return res.status(401).send('Unauthorized Request')
+    }
+    let token = req.headers.authorization.split(' ')[1]
+    if (token === 'null') {
+        return res.status(401).send('Unauthorized Request')
+    }
+    let payload = jwt.verify(token, 'secretKey')
+    if (!payload) {
+        return res.status(401).send('Unauthorized Request')
+    }
+    req.userId = payload.subject
+    next()
+}
+
+router.get('/', (req, res) => {
     res.send('From API route')
+})
+
+router.post('/register', (req, res) => {
+    let userData = req.body
+    let user = new User(userData)
+    user.save((error, registeredUser) => {
+        if (error) {
+            console.log(error)
+        } else {
+            let payload = { subject: registeredUser._id }
+            let token = jwt.sign(payload, 'secretKey')
+            res.status(200).send({ token })
+        }
+    })
+})
+
+router.post('/login', (req, res) => {
+    let userData = req.body
+
+    User.findOne({ email: userData.email }, (error, user) => {
+        if (error) {
+            console.log(error)
+        } else {
+            if (!user) {
+                res.status(401).send('Invalid email or password')
+            } else
+                if (user.password !== userData.password) {
+                    res.status(401).send('Invalid email or password')
+                } else {
+                    let payload = { subject: user._id }
+                    let token = jwt.sign(payload, 'secretKey')
+                    res.status(200).send({ token })
+                }
+        }
+    })
+})
+
+router.get('/events', (req, res) => {
+    let events = [
+        {
+            "_id": "1",
+            "name": "Auto Expo",
+            "description": "lorem ipsum",
+            "date": "2012-04-23T18:25:43.511Z"
+        },
+        {
+            "_id": "2",
+            "name": "Auto Expo",
+            "description": "lorem ipsum",
+            "date": "2012-04-23T18:25:43.511Z"
+        }
+    ]
+    res.json(events)
+})
+
+router.get('/special', verifyToken, (req, res) => {
+    let events = [
+        {
+            "_id": "1",
+            "name": "Auto Expo",
+            "description": "lorem ipsum",
+            "date": "2012-04-23T18:25:43.511Z"
+        },
+        {
+            "_id": "2",
+            "name": "Auto Expo",
+            "description": "lorem ipsum",
+            "date": "2012-04-23T18:25:43.511Z"
+        }
+    ]
+    res.json(events)
 })
 
 module.exports = router
